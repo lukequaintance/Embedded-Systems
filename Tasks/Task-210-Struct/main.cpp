548b7b88ff5670e55f9754fb19f71ccc6b02efbc
lukequaintance <luke.quaintance@students.plymouth.ac.uk>
2023-09-29 16:23:23 +0100

    todays lab
    lab

--- Tasks/Task-210-Struct/main.cpp
+++ Tasks/Task-210-Struct/main.cpp
@@ -21,8 +21,18 @@ int main() {
 
     // TASK:
     // Create another complex number y
+    ComplexNumber_C y;
+    
+    y.real = 0.0;
+    y.imag = 0.0;
     // Calculate the complex sum of p and q and store in y
+    y.real = (p.real + q.real);
+    y.imag = (p.imag + q.imag);
+    // y = (q + p);
+     
     // Use printf to display as a complex number (hint: you need two placeholders)
+    printf ("%d\n", y);
+
     
     while (true) {
     }

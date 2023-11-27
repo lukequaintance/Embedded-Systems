2e7f5c6aa9910b734a6d3ec98c3f9239782032d4
lukequaintance <luke.quaintance@students.plymouth.ac.uk>
2023-11-27 16:54:35 +0000

    uh

--- Tasks/Task-390-TCP-Client/main.cpp
+++ Tasks/Task-390-TCP-Client/main.cpp
@@ -9,7 +9,7 @@
 EthernetInterface net;
 char rbuffer[65];
 
-#define IPV4_HOST_ADDRESS "10.42.0.1"
+#define IPV4_HOST_ADDRESS "192.168.137.1"
 #define TCP_SOCKET_PORT 8888
 
 DigitalIn BlueButton(USER_BUTTON);
